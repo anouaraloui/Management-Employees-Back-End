834fb42b868541b50cbf1cbf71ee6d15401fa70c
anouaraloui <alouianwer21@gmail.com>
2023-02-13 23:07:19 +0100

    Second Commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,11 +8,11 @@ export const isAuth = (req, res, next) => {
         const token = req.headers.authorization.split(' ')[1]; 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN); 
         const userId = decodedToken.userId; 
-        if (req.body.userId && req.body.userId !== userId) { 
-            throw 'Invalid user ID'; 
-        } else { 
-            next(); 
-        } 
+        if (req.body.userId && req.body.userId !== userId)  throw 'Invalid user ID';
+            
+         else  next(); 
+           
+         
     } 
     catch { 
         res.status(401).json({ error: new Error('Invalid request!') }); 
