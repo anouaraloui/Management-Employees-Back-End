6953c9c5677e92edc006662cc77c05179792b1f0
anouaraloui <alouianwer21@gmail.com>
2023-02-17 22:41:48 +0100

    Second Update

--- routes/daysOffRoutes.js
+++ routes/daysOffRoutes.js
@@ -8,27 +8,27 @@ import { validateRequestDaysOff, validateRequestDecision } from "../middlewares/
 const router = express.Router();
 
 // Route for added a new request days off
-router.post("/daysOff", isAuth,(req, res, next)=> checkRole(['Super Admin','Director', 'Administration Director', 'Administration Assistant', 'Team Manager', 'Software Enginner'], req, res, next),
+router.post("/daysOff", isAuth,(req, res, next)=> checkRole(['Super Admin','Director', 'Administration Director', 'Administration Assistant', 'Team Manager', 'Software Engineer'], req, res, next),
 validateRequestDaysOff, addDaysOff)
 
 // Route for the display all request of days off
-router.get('/daysOff', isAuth, (req, res, next)=> checkRole(['Super Admin','Director', 'Administration Director', 'Administration Assistant', 'Team Manager', 'Software Enginner'], req, res, next),
+router.get('/daysOff', isAuth, (req, res, next)=> checkRole(['Super Admin','Director', 'Administration Director', 'Administration Assistant', 'Team Manager', 'Software Engineer'], req, res, next),
 getDaysOff)
 
 // route for displaying the information of a request of days off whose identifier is known
-router.get('/daysOff/:id', isAuth,(req, res, next)=> checkRole(['Super Admin','Director', 'Administration Director', 'Administration Assistant', 'Team Manager', 'Software Enginner'], req, res, next),
+router.get('/daysOff/:id', isAuth,(req, res, next)=> checkRole(['Super Admin','Director', 'Administration Director', 'Administration Assistant', 'Team Manager', 'Software Engineer'], req, res, next),
 validorId ,getDaysOffById)
 
 // Route for deletion of a well-defined request of days off
-router.delete("/delete/:id", isAuth,(req, res, next)=> checkRole(['Super Admin','Director', 'Administration Director', 'Administration Assistant', 'Team Manager', 'Software Enginner'], req, res, next),
+router.delete("/delete/:id", isAuth,(req, res, next)=> checkRole(['Super Admin','Director', 'Administration Director', 'Administration Assistant', 'Team Manager', 'Software Engineer'], req, res, next),
 validorId ,deleteDaysOff)
 
 // Route for delete all request of days off
-router.delete("/deleteAll", isAuth,(req, res, next)=> checkRole(['Super Admin','Director', 'Administration Director', 'Administration Assistant', 'Team Manager', 'Software Enginner'], req, res, next),
+router.delete("/deleteAll", isAuth,(req, res, next)=> checkRole(['Super Admin','Director', 'Administration Director', 'Administration Assistant', 'Team Manager', 'Software Engineer'], req, res, next),
 deleteAllDaysOff)
 
 // Updating a request of days off for which the identifier is known
-router.put("/daysOff/:id", isAuth, (req, res, next)=> checkRole(['Super Admin','Director', 'Administration Director', 'Administration Assistant', 'Team Manager', 'Software Enginner'], req, res, next),
+router.put("/daysOff/:id", isAuth, (req, res, next)=> checkRole(['Super Admin','Director', 'Administration Director', 'Administration Assistant', 'Team Manager', 'Software Engineer'], req, res, next),
 validorId ,updateDaysOff)
 
 // Route for decision of days off
