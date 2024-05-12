90c30f5b6d6803d3cde6af18871ffc99cb77d7f6
anouaraloui <alouianwer21@gmail.com>
2024-05-12 11:45:18 +0100

    New Updated

--- middlewares/checkRole.js
+++ middlewares/checkRole.js
@@ -7,5 +7,5 @@ export const checkRole= (roles, req, res, next)=> {
         
     const token = req.headers.authorization.split(' ')[1]; 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN); 
-    (roles.includes(decodedToken.role)) ? next() : res.status(403).json({error: 'access denieted'})
+    (roles.includes(decodedToken.role)) ? next() : res.status(403).json({error: 'Access denied'})
 };
